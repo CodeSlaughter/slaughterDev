318757b982bf8f08f3c54673e8b64724da4c5fb7
Iam Slaughter <jlgslaughter@gmail.com>
2018-05-31 19:57:33 -0400

    slaugherdev 2.0

--- app.js
+++ app.js
@@ -9,8 +9,16 @@ const port = process.env.PORT || 8080
 app.use(express.static(path.join(__dirname, '/public')))
 
 //Route
+app.get('/package', (req, res) => {
+    res.sendFile(path.join(__dirname+ '/views/package.html'))
+})
+
+app.get('/developer', (req, res) => {
+    res.sendFile(path.join(__dirname+ '/views/developer.html'))
+})
+
 app.get('/', (req, res) => {
-    res.sendFile('index.html')
+    res.sendFile(path.join(__dirname+ '/views/split.html'))
 })
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
